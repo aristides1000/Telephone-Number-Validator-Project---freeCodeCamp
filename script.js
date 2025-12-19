1e0c7defa793adf0fd0c44356c7db3426c192904
aristides1000 <aristides1000@gmail.com>
2025-12-19 17:02:25 -0400

    Feat: Finish all necessary logic requierments

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ const checkNum = number => {
   if (!number) {
     alert('Please provide a phone number');
   }
+
+  const regEx = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+  resultsDiv.textContent = regEx.test(number) ?
+  `Valid US number: ${number}` :
+  `Invalid US number: ${number}`
 }
 
 checkBtn.addEventListener('click', function () {
   event.preventDefault();
   checkNum(userInput.value);
+});
+
+clearBtn.addEventListener('click', function() {
+  event.preventDefault();
+  resultsDiv.textContent = '';
 });
